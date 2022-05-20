70c97ced8bb04b9e37472f62acc619866998d3c1
Gerardo Miranda <gerardodev25@gmail.com>
2022-05-20 16:19:42 -0400

    init minichat

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-io.on('connection', () => {});
+app.use(express.static('./public'));
+
+io.on('connection', () => {
+  console.log('client connected');
+});
 
 server.listen(8080, () => {
   console.log('server up in 8080 port');
